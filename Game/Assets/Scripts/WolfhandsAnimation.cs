a8ba706c7b5c1d32a6b9757d27385f4ec794145a
Juutis <>
2022-01-29 13:46:31 +0200

    Added wolfhands animations

---
+++ Game/Assets/Scripts/WolfhandsAnimation.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfhandsAnimation : MonoBehaviour
+{
+    public bool TriggerQuickAttack = false;
+    public bool TriggerPowerAttack = false;
+
+    private Animator anim;
+
+    private string animAttackProp = "Attack";
+    private int quickAttacks = 2;
+    private int powerAttacks = 1;
+    private bool isAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TriggerQuickAttack) {
+            TriggerQuickAttack = false;
+            QuickAttack();
+        }
+        if (TriggerPowerAttack) {
+            TriggerPowerAttack = false;
+            PowerAttack();
+        }
+    }
+
+    public void PowerAttack() {
+        triggerAttack(true);
+    }
+
+    public void QuickAttack() {
+        triggerAttack(false);
+    }
+
+    private void triggerAttack(bool isPowerAttack) {
+        if (isAttacking) return;
+
+        var attackIndex = 0;
+        if (isPowerAttack) {
+            attackIndex = 10 + Random.Range(0, powerAttacks);
+        } else {
+            attackIndex = 1 + Random.Range(0, quickAttacks);
+        }
+        anim.SetInteger(animAttackProp, attackIndex);
+        isAttacking = true;
+        Invoke("ResetAttacking", 0.25f);
+    }
+
+    public void ResetAttacking() {
+        isAttacking = false;
+        anim.SetInteger(animAttackProp, 0);
+    }
+
+    public bool ReadyToAttack() {
+        return !isAttacking;
+    }
+}
